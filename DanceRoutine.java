9dd9df1df249028300092ac9d640086541bd56e0
KPRMODI <kpmodi@my.waketech.edu>
2025-10-29 09:08:20 -0400

    made loops better

--- DanceRoutine.java
+++ DanceRoutine.java
@@ -7,16 +7,16 @@ public class DanceRoutine {
 
     public String buildRoutine(){
         StringBuilder routine = new StringBuilder(); // string  builder
-        for (int i = 0; i < 3; i++) { // iterates through list of 3 
-            routine.append(boohbahs[i].performMove() + "\n"); //appends boobahs to string builder
+        for (Boohbah b: boohbahs) { // iterates through list using enchanced loop
+            routine.append(b.performMove() + "\n"); //appends boobahs to string builder
         }
         return routine.toString(); //converts to string
     }
 
     public String modifyRoutine(){
         StringBuilder modifiedRoutine = new StringBuilder();
-        for (int i = 0; i < 3; i++) {
-            modifiedRoutine.append(boohbahs[i].performMove() + "\n");
+        for (Boohbah b: boohbahs) {
+            modifiedRoutine.append(b.performMove() + "\n");
     }
     String Mod = modifiedRoutine.toString().replace("performs", "dances to");//converts string builder to string
     return Mod; //returns modified version
